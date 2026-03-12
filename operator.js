4ee504bf45bdea875bb02133f5e744f3e91c30ea
Santhiya <santhiyameena2482@gmail.com>
2026-03-12 13:10:35 +0530

    operator(11/03/2026)

---
+++ operator.js
@@ -0,0 +1,50 @@
+console.log("ARITHMETIC OPERATOR:");
+let a=10;
+let b=20;
+let c;
+c=a+b;
+console.log("addition:",c);
+
+let d=20;
+let e=10;
+let f=d-e;
+console.log("subraction",f);
+
+let g=50,h=80,i;
+i=g*h;
+console.log("multiplication",i);
+
+
+let j=40,k=2,l;
+l=j/k;
+console.log("division",l);
+
+
+let m=40,n=4,o;
+o=m%n;
+console.log("module division",o);
+
+console.log("COMPARISON OPERATOR:");
+ 
+ let x=10;
+ if(x >18){
+   console.log("you are eligible for vote");
+ }
+ else{
+   console.log("your not eligible for vote");
+ }
+
+ console.log("ASSIGNMENT OPERATOR:  ");
+ let y=10; w=11;
+ y+=2;
+ y-=2;
+ y*=2;
+ y%=2;
+ y/=2;
+ console.log(y);
+
+console.log("LOGICAL OPERATOR:")
+let z=5,s=10;
+console.log("AND operator:",z<=s && z>=s);
+console.log("OR operator:",z==s|| z==s);
+console.log("Not operator:",z>=s != z<=s );
