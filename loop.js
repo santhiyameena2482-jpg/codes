5ef20e56c177be5eec2c9f380e109679a770fe94
Santhiya <santhiyameena2482@gmail.com>
2026-03-12 13:14:48 +0530

    loop(11/03/2026)

---
+++ loop.js
@@ -0,0 +1,31 @@
+// for loop
+let fr=[2,3,4,5];
+for(i=0;i<fr.length;i++){
+    console.log(fr[i]);
+};
+
+//for of loop
+
+let for2=[4,5,6,7];
+ for(a of for2){
+    console.log(a);
+ };
+
+ //while loop
+
+ let while1=[6,7,8,9,2];
+ let e=0;
+ while(e<while1.length){
+    console.log(while1[e]);
+    e++;
+ };
+
+
+ //do while
+
+ let do1=10;
+  do{
+    console.log(do1);
+    do1++;
+  }while(do1<=9)
+  
