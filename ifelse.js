2f868609c42ec732b6c550ffd5275725c04dff60
Santhiya <santhiyameena2482@gmail.com>
2026-03-12 13:09:25 +0530

    if else(11/03/2026)

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+let a=35;
+if(a>=90){
+    console.log("grade:a");
+}
+else if(a>=65){
+    console.log("grade:b");
+}
+else if(a>=35){
+    console.log("grade:c")
+} 
+else{
+    console.log("work hard to next time")
+}
+
