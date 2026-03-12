1ee531821ef47a6d3f42e2b3fe6bee020e61224d
Santhiya <santhiyameena2482@gmail.com>
2026-03-12 13:15:44 +0530

    switch(11/03/2026)

---
+++ switch.js
@@ -0,0 +1,25 @@
+let day="Wednesday";
+switch(day){
+    case "Monday":
+        console.log("Monday");
+        break;
+    case "Tuesday":
+        console.log("Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Wednesday");
+        break;
+    case "Thursday":
+        console.log("Thursday");
+        break;
+    case "Friday":
+        console.log("Friday");
+        break;
+    case "Saturday":
+        console.log("Saturday");
+        break;
+    case "Sunday":
+        console.log("Sunday");
+        break;
+    
+};
