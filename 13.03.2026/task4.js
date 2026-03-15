721d1c9e0909a5b46aca381258b42df91b1985a5
Santhiya <santhiyameena2482@gmail.com>
2026-03-15 20:55:21 +0530

    Comparison logic(13.03.26)

---
+++ 13.03.2026/task4.js
@@ -0,0 +1,4 @@
+console.log([] == false) // empty array=false , so false == false => true.
+console.log([] === false)// === operator check both values and datatype ,
+// array is non-primitive datatype and false is boolean type so, reference === value => false.
+//  so it gives false.
