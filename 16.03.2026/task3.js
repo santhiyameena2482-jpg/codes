3df9aec4d9195f6a742c9ed27ffb23f24cb9eaf0
Santhiya <santhiyameena2482@gmail.com>
2026-03-16 22:49:00 +0530

    The Multi-Parameter Arrow Conversion
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ 16.03.2026/task3.js
@@ -0,0 +1,13 @@
+//function add(a,b){
+//     return a+b;
+// }
+// console.log("the addition value is:",add(5,6));
+
+// const add=function(a,b){
+//     return a+b;
+// }
+// console.log(add(5,6));
+
+const add=(a,b)=>a+b;
+console.log(add(5,6));
+//Answer for challenge: because we skip first parameter that contain current element, second parameter contain current element index, so it gives index of the element that is [ 0, 3, 4, 9].
