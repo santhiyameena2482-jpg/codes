27beb0361f679bba1814e1a553796b432f87d258
Santhiya <santhiyameena2482@gmail.com>
2026-03-16 22:53:30 +0530

    The const Mutation Rule
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ 16.03.2026/task6.js
@@ -0,0 +1,10 @@
+//  const colors = ["Red", "Blue"];
+//    colors.push("Green");
+//    console.log(colors);
+   
+ const colors = ["Red", "Blue"];
+   colors.push("Green");
+   colors=["Yellow"];
+   console.log(colors);
+   
+   //first one is work because it only add the element to array not change its entire array .but second one change whole array value, so it gives TypeError that means array only store reference.
