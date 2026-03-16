afccb8d2ac399727cc44c84cedf896097904c04b
Santhiya <santhiyameena2482@gmail.com>
2026-03-16 22:52:22 +0530

    Comparison logic:the Coercion Chain
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ 16.03.2026/task5.js
@@ -0,0 +1,16 @@
+// array=[1,2,3,4];
+// array.map((e,i)=>{
+//  console.log(`element in position ${i} : ${e}`);
+// });
+
+
+// array=[1,2,3,4];
+// array.map(function el_in(e,i){
+//    console.log(`element in position ${i} : ${e}`);
+// });
+
+
+array=[1,2,3,4];
+const el_in=array.map(function(e,i){
+console.log(`element in position ${i} : ${e}`);
+});
