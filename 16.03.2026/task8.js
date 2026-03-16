6f2683f8d01f08a0dadaa13d0648a7f429c4298c
Santhiya <santhiyameena2482@gmail.com>
2026-03-16 22:55:18 +0530

    The Switch "Fall-through" Logic
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ 16.03.2026/task8.js
@@ -0,0 +1,8 @@
+let grade="A";
+switch(grade){
+    case "A":
+        console.log("Excellent");
+    case "B":
+        console.log("Good");
+}
+//because not use break statement so it print both Excellent and Good.
